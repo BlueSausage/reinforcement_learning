ad35c7c77f5af8a10275b7cfb1f889ed5439a13a
Niklas Schneider <36741399+BlueSausage@users.noreply.github.com>
2025-02-04 10:00:31 +0100

    changed the hidden_size to a list

--- reinforce/model.py
+++ reinforce/model.py
@@ -8,29 +8,26 @@ class PolicyNetwork(nn.Module):
         Args:
             state_size (int): Size of each state.
             action_size (int): Size of each action.
-            hidden_size (int): Size of hidden layers.
+            hidden_size (list): List of hidden layer sizes.
         """
         
         super(PolicyNetwork, self).__init__()
         
         self.input_layer = nn.Sequential(
-            nn.Linear(state_size, hidden_size),
+            nn.Linear(state_size, hidden_size[0]),
             nn.ReLU()
         )
         
-        self.hidden_layer1 = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU()
-        )
-        
-        self.hidden_layer2 = nn.Sequential(
-            nn.Linear(hidden_size, hidden_size),
-            nn.ReLU()
-        )
-        
+        self.hidden_layers = nn.ModuleList([
+            nn.Sequential(
+                nn.Linear(hidden_size[i], hidden_size[i+1]),
+                nn.ReLU()
+            ) for i in range(len(hidden_size)-1)
+        ])
+
         self.output_layer = nn.Sequential(
-            nn.Linear(hidden_size, action_size),
-            nn.Softmax(dim=1) # dim=-1 gets the last dimensio of a tensor instead dimension 1 (dim=1) 
+            nn.Linear(hidden_size[-1], action_size),
+            nn.Softmax(dim=-1) # dim=-1 gets the last dimension of a tensor, instead of dimension 1 (dim=1) 
         )
         
         
@@ -45,8 +42,8 @@ class PolicyNetwork(nn.Module):
         """
         
         x = self.input_layer(x)
-        x = self.hidden_layer1(x)
-        x = self.hidden_layer2(x)
+        for layer in self.hidden_layers:
+            x = layer(x)
         x = self.output_layer(x)
         
         return x
